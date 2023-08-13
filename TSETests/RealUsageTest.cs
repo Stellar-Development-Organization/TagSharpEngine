5a6ede54c3e775e9b3bcdb6d00cf4ab71a82dda5
RealKiller666 <nabdev0777@gmail.com>
2023-08-14 01:06:47 +0700

    DiscordAdapter update and fixed stuff.

--- TSETests/RealUsageTest.cs
+++ TSETests/RealUsageTest.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using TagSharpEngine;
 
@@ -10,6 +11,26 @@ namespace TSETests {
 
             var finalize = $"{verb_var.Declaration}({verb_var.Parameter}):{verb_var.Payload}";
             Console.WriteLine(finalize);
+        }
+    }
+
+    [TestClass]
+    public class TestExample {
+        [TestMethod("Basic Verb Test")]
+        public void TestBasicVerb() {
+            var verb_var = new Verb("{hello:world}");
+            Assert.IsInstanceOfType(verb_var, typeof(Verb));
+
+            var bare_verb = new Verb("{test}");
+            Console.WriteLine(bare_verb.ToString());
+            Assert.AreEqual(bare_verb.Parameter, null);
+            Assert.AreEqual(bare_verb.Payload, null);
+            Assert.AreEqual(bare_verb.Declaration, "test");
+
+            var another_bare_verb = new Verb("{user(hello)}");
+            Assert.AreEqual(another_bare_verb.Parameter, "hello");
+            Assert.AreEqual(another_bare_verb.Payload, null);
+            Assert.AreEqual(another_bare_verb.Declaration, "user");
         }
     }
 }
--- TagSharpEngine/Interface/Adapter.cs
+++ TagSharpEngine/Interface/Adapter.cs
@@ -2,9 +2,7 @@
     /// <summary>
     /// An abstract base class for creating your own tag block.
     /// </summary>
-    public abstract class Adapter {
-        public virtual string? GetValue(Context ctx) {
-            throw new NotImplementedException();
-        }
+    public interface IAdapter {
+        public virtual string? GetValue(Context ctx) => throw new NotImplementedException();
     }
 }
